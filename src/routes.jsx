983ae2c3bae5aa5070f8ee738943b60551b74d9f
Nathally Souza <nathallyccd@gmail.com>
2020-12-09 08:30:57 -0300

    delete pokedex

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './Views/index';
 import AboutPage from './Views/about';
 import NotFound from './Views/notfound';
-import Pokedex from './components/Pokedex/index'
 
 const MainRoutes = () => {
     return(
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/pokedex" element={<Pokedex/>} />
             <Route path="*" element={<NotFound/>} />
         </Routes>
     )
