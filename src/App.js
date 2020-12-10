7c510f091eb9bece9ec7f3dd341b2a6774614db8
Nathally Souza <nathallyccd@gmail.com>
2020-12-10 15:40:34 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <Header/>
       <Routes/>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <>
+        <div className="bg-dark text-center m-2">
+            <p>&copy; Nathally Souza</p>
+        </div>
+        </>
+    )
+}
+
+export default Footer;
