573e89df3457a81afbe5bae5e9622c204d3ec72b
nathyts <nathallyccd@gmail.com>
2020-12-29 14:42:11 -0300

    post 3

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Post1 from './post1';
 import Post2 from './post2';
+import Post3 from './post3';
 import Jumbotron from '../Home/Jumbotron';
 
 const Blog = () => {
   return (
       <div className="row">
         <div className="col-lg-9">
+          <Post3/>
           <Post2/>
           <Post1/>
         </div>
